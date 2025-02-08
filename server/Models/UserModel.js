52b911c45e2f3e022157e168903a90f64574e6fc
shivadhanush1216 <shivadhanushreddythaduri@gmail.com>
2025-02-08 23:51:08 +0530

    Replace bcrypt with bcryptjs

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -28,4 +28,4 @@ userSchema.pre("save", async function (next) {
     next();
 })
 
-module.exports = mongoose.model("user",userSchema)
+module.exports = mongoose.model("user", userSchema)
